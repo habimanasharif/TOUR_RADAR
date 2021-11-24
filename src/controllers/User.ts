b5f254fd431d68b8e655930108fcf3b222a8dbbc
sharifhabiman <hdidiersharif@gmail.com>
2021-11-25 00:03:53 +0200

    finishes implimenting sending verification email

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -2,15 +2,24 @@ import Joi from "@hapi/joi"
 import { sign, verify } from '../helpers/jwt';
 import { generate, check } from '../helpers/bcrypt';
 import { UserService } from '../database/services/users';
+import {mailer} from '../helpers/mailer'
 import { UserInputError } from 'apollo-server';
 import * as Validations from '../middleware/validation/user';
+import config from '../config';
  class User{
   static async signUp(parent:any,{firstname,lastname,username,password,email,role}:{firstname:string,lastname:string,username:string,password:string,email:string,role:string},ctx:any) {
-   const exists = await UserService.findUser({email:email});
+   const exists = await UserService.findUser({ $or: [{ email }, { username }] });
    if(exists){
     throw new UserInputError("USER EXISTS ERROR");
    }
-Validations.signUp({firstname,lastname,username,password,email});
+ Validations.signUp({firstname,lastname,username,password,email});
+await mailer(['sign-up', {
+  email,
+  firstname,
+  lastname,
+  body: `${config.HOST}/user/verify?token=${sign(email)}`
+}, email
+]);
     const hashedpassword= await generate(password);
     let newUser ={
     firstname,
@@ -20,7 +29,9 @@ Validations.signUp({firstname,lastname,username,password,email});
     role,
     email,
     }
+
     const newAcount=UserService.signUp(newUser);
+    
     return newAcount;
     
   }
