930ab11e73859df61ce9606dfe657c61f5cc69ed
sharifhabiman <hdidiersharif@gmail.com>
2022-01-01 13:42:20 +0200

    feature user unlike Post [starts #180760076]

--- src/controllers/Posts.ts
+++ src/controllers/Posts.ts
@@ -80,5 +80,15 @@ class Post {
     post.likes = like;
     return post;
   }
+
+  static async UnLikePost(parent:any, { postId }:{postId:string}, ctx:any) {
+    const id = await isUser(ctx);
+    const post = await PostService.fetchSinglePost(postId);
+    const user = await UserService.findUser({ _id: id });
+    if (!user) throw new UserInputError('User Not found');
+    if (!post) throw new UserInputError('Post Not Found');
+    const likeExist = await LikeService.findLike(id, post);
+    if (likeExist.length < 0) throw new UserInputError('You Can not unlike a non liked PostPost ');
+  }
 }
 export { Post };
