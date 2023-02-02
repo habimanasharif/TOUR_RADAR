cdf119e9c2d03d51328679661cd9ed26c7e2e51b
sharif Habiman <hdidiersharif@gmail.com>
2023-02-02 22:14:30 +0200

    removed dist and updated  typescript version

--- dist/helpers/extention.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/* eslint-disable import/prefer-default-export */
-exports.default = (file) => {
-    const ext = file.split('.').pop();
-    return ext;
-};
--- dist/typeDefs/types/messageType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const apollo_server_1 = require("apollo-server");
-const MessageType = (0, apollo_server_1.gql) `
-  type Message {
-      message:String
-  }
-  `;
-exports.default = MessageType;
